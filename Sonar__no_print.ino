552ddcea5ca6efa71a585f1d780526be0984d6c5
Prajwal Sood <53113703+PrajwalSood@users.noreply.github.com>
2019-10-31 11:31:36 +0530

    Updated Sonar code
    Removing print lines to avoid unnecessary processing if only led are used

---
+++ Sonar__no_print.ino
@@ -0,0 +1,50 @@
+const int trigpin = 12; 
+const int echopin = 11;
+const int l = 13;
+const int l1 = 8;
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigpin, OUTPUT);
+  pinMode(echopin, INPUT);
+
+}
+
+void loop() {
+  long duration, inches, cm;
+  digitalWrite(trigpin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigpin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigpin, LOW);
+
+  duration = pulseIn(echopin, HIGH);
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+  if (cm < 5){
+    digitalWrite(l, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(l, LOW);
+  }
+  else if (cm < 10){
+    digitalWrite(l1, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(l1, LOW);
+  }
+   else{
+    digitalWrite(l, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(l, LOW);
+    digitalWrite(l1, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(l1, LOW);
+   
+  }
+}
+
+long microsecondsToInches(long microseconds){
+  return microseconds/74/2;
+}
+
+long microsecondsToCentimeters(long microseconds){
+  return microseconds/29/2;
+}
